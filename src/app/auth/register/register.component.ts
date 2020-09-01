1958a478882430d5c17d6f0735f46d91f17030a2
Andres Palacios <andrespvzla@gmail.com>
2020-09-01 01:47:01 -0400

    Login y Auth Frontend

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Component } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { UsuarioService } from './../../services/usuario.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-register',
@@ -7,11 +11,72 @@ import { Component, OnInit } from '@angular/core';
     './register.component.css'
   ]
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
 
-  constructor() { }
+  public formSubmitted = false;
 
-  ngOnInit(): void {
+  public registerForm = this.fb.group({
+    nombre: ['', [Validators.required, Validators.minLength(3)]],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(3)]],
+    password2: ['', [Validators.required, Validators.minLength(3)]],
+    terminos: [false, Validators.required]
+  }, {
+    validators: this.passwordIguales('password', 'password2')
+  });
+
+  constructor(private fb: FormBuilder,
+              private usuarioService: UsuarioService,
+              private router: Router
+  ) { }
+
+  crearUsuario() {
+    this.formSubmitted = true;
+    if (this.registerForm.invalid) {
+      return;
+    }
+    this.usuarioService.crearUsuario(this.registerForm.value).subscribe(resp => {
+      this.router.navigateByUrl('/');
+      }, (err) => {
+        Swal.fire('Error', err.error.msg, 'error');
+      }
+    );
+  }
+
+  contrasenasNoValidas() {
+    const pass1 = this.registerForm.get('password').value;
+    const pass2 = this.registerForm.get('password2').value;
+
+    if (pass1 !== pass2 && this.formSubmitted) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  campoNoValido(campo: string): boolean {
+    if (this.registerForm.get(campo).invalid && this.formSubmitted) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  aceptaTerminos() {
+    return !this.registerForm.get('terminos').value && this.formSubmitted;
+  }
+
+  passwordIguales(pass1Name: string, pass2Name: string) {
+    return (formGroup: FormGroup) => {
+      const pass1Control = formGroup.get(pass1Name);
+      const pass2Control = formGroup.get(pass2Name);
+
+      if (pass1Control.value === pass2Control.value) {
+        pass2Control.setErrors(null);
+      } else {
+        pass2Control.setErrors({ noEsIgual: true});
+      }
+    }
   }
 
 }
--- src/app/pages/pages.routing.ts
+++ src/app/pages/pages.routing.ts
@@ -1,5 +1,6 @@
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
+import { AuthGuard } from './../guards/auth.guard';
 
 import { PagesComponent } from './pages.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
@@ -13,6 +14,7 @@ const routes: Routes = [
    {
       path: 'dashboard',
       component: PagesComponent,
+      canActivate: [AuthGuard],
       children: [
         { path: '', component: DashboardComponent, data: {titulo: 'Dashboard' } },
         { path: 'progress', component: ProgressComponent, data: {titulo: 'Progress' } },
