3e36dbb571e9eccc035905c488c7a6e223ebbb03
Andres Palacios <andrespvzla@gmail.com>
2020-10-08 02:09:37 -0400

    Fin sección 15

--- src/app/pages/pages.routing.ts
+++ src/app/pages/pages.routing.ts
@@ -9,6 +9,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { AccountSettingsComponent } from './account-settings/account-settings.component';
 import { PromesasComponent } from './promesas/promesas.component';
 import { RxjsComponent } from './rxjs/rxjs.component';
+import { PerfilComponent } from './perfil/perfil.component';
 
 const routes: Routes = [
    {
@@ -21,7 +22,8 @@ const routes: Routes = [
         { path: 'grafica1', component: Grafica1Component, data: {titulo: 'Graficas' } },
         { path: 'account-settings', component: AccountSettingsComponent, data: {titulo: 'Ajustes de cuenta' } },
         { path: 'promesas', component: PromesasComponent, data: {titulo: 'Promesas' } },
-        { path: 'rxjs', component: RxjsComponent, data: {titulo: 'Rxjs' } }
+        { path: 'rxjs', component: RxjsComponent, data: {titulo: 'Rxjs' } },
+        { path: 'perfil', component: PerfilComponent, data: {titulo: 'Perfil de usuario' } }
       ]
     },
 ];
--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -9,6 +9,8 @@ import { RegisterForm } from './../interfaces/register-form.interface';
 import { LoginForm } from './../interfaces/login-form.interface';
 import { Observable, of } from 'rxjs';
 
+import { Usuario } from './../models/usuario.model';
+
 const base_url = environment.base_url;
 declare const gapi: any;
 
@@ -18,9 +20,18 @@ declare const gapi: any;
 export class UsuarioService {
 
   public auth2: any;
+  public usuario: Usuario;
 
   constructor(private http: HttpClient, private router: Router, private ngZone: NgZone) {
     this.googleInit();
+  }
+
+  get token(): string {
+    return localStorage.getItem('token') || '';
+  }
+
+  get uid(): string {
+    return this.usuario.uid || '';
   }
 
   logout() {
@@ -45,16 +56,17 @@ export class UsuarioService {
   }
 
   validarToken(): Observable<boolean> {
-    const token = localStorage.getItem('token') || '';
     return this.http.get(`${base_url}/login/renew`, {
       headers: {
-        'x-token': token
+        'x-token': this.token
       }
     }).pipe(
-      tap((resp: any)=> {
+      map((resp: any) => {
+        const { nombre, email, google, img = '', role, uid } = resp.usuario;
+        this.usuario = new Usuario(nombre, email, '', google, img, role, uid);
         localStorage.setItem('token', resp.token);
+        return true;
       }),
-      map(resp => true),
       catchError(error => of(false))
     );
   }
@@ -62,10 +74,22 @@ export class UsuarioService {
   crearUsuario(formData: RegisterForm) {
     return this.http.post(`${base_url}/usuarios`, formData)
       .pipe(
-        tap((resp:any) => {
+        tap((resp: any) => {
           localStorage.setItem('token', resp.token);
         })
       );
+  }
+
+  actualizarPerfil(data: {email: string, nombre: string, role: string}) {
+    data = {
+      ...data,
+      role: this.usuario.role
+    }
+    return this.http.put(`${base_url}/usuarios/${this.uid}`, data, {
+      headers: {
+        'x-token': this.token
+      }
+    });
   }
 
   login(formData: LoginForm) {
